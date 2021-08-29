f71b7a43a6f6c54ae4c8fc37e550cd5a6e3c610e
Niccari <latieos93@gmail.com>
2021-08-29 14:20:19 +0900

    Fix: build.sh variables

--- proto/build.sh
+++ proto/build.sh
@@ -6,12 +6,12 @@ lib_path="./def_proto"
 python3 -m grpc_tools.protoc $proto_name.proto --proto_path=. --python_out=$lib_path --grpc_python_out=$lib_path
 
 # make (proto_name)_pb2_grpc importable.
-sed -i '' -e 's/^import api_pb2/from . import api_pb2/' "$lib_path/${proto_name}_pb2_grpc.py"
+sed -i '' -e "s/^import ${proto_name}_pb2/from . import ${proto_name}_pb2/" "$lib_path/${proto_name}_pb2_grpc.py"
 
 python3 setup.py bdist_wheel
-rm -r build def_proto.egg-info
+rm -r build *.egg-info
 cp dist/*.whl ../client
 cp dist/*.whl ../server
 rm -r dist
 
-echo "Success! def_proto*.whl has been written in server/client directory."
+echo "Success! libraries has been written in server/client directory."
